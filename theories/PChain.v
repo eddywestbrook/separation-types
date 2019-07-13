2675042ff57fbe8ef0200651d90d39005f3a18a0
Eddy Westbrook <westbrook@galois.com>
2019-07-12 17:47:14 -0700

    changed diagrams to include embed as well as projection funs

--- theories/PChain.v
+++ theories/PChain.v
@@ -6,21 +6,35 @@ Require Import SepTypes.OrderedType SepTypes.OFuns.
 Open Scope ofun_scope.
 
 
-(** Diagrams in the category of ordered types. *)
-Section pDiagram.
-  Class OTypeSequence (f : nat -> Type) : Type :=
-    oTypeSeq :> forall n, OType (f n).
-  Class PDiagram (f : nat -> Type) {o : OTypeSequence f} : Type :=
-    proj : forall n, f (S n) -o> f n.
+(* An ordered type sequence is simply an ordered type for each nat *)
+Class OTypeSequence (f : nat -> Type) : Type :=
+  oTypeSeq : forall n, OType (f n).
 
-  (** "Projective limit" chains. *)
-  Record PChain f `{PDiagram f} : Type :=
-    { chain : forall n, f n
-    ; chainCondition : forall n, chain n =o= proj n @@ (chain (S n)) }.
-End pDiagram.
+Ltac otype_for_sequence :=
+  match goal with
+    o : OTypeSequence ?f |- OType (?f ?n) => apply o
+  end.
+
+Hint Extern 1 (OType _) => otype_for_sequence : typeclass_instances.
+
+(* A diagram (in the category of embedding-projection pairs) is an ordered
+   type sequence with embedded-projection pairs between them *)
+Class PDiagram (f : nat -> Type) {o : OTypeSequence f} : Type :=
+  { embed : forall n, f n -o> f (S n);
+    proj : forall n, f (S n) -o> f n;
+    embed_proj_eq : forall n, compose_ofun (embed n) (proj n) =o= id_ofun;
+    proj_embed_leq : forall n, compose_ofun (proj n) (embed n) <o= id_ofun
+  }.
+
+(* A projective limit chain is a sequence of elements of an ordered type
+   sequence where each element is the projection of the next one *)
+Record PChain f `{PDiagram f} : Type :=
+  { chain : forall n, f n;
+    chainCondition : forall n, chain n =o= proj n @@ (chain (S n)) }.
 
 Arguments chain {_ _ _}.
 
+(* Two chains are related iff they are related pointwise *)
 Instance OTPChain f `{PDiagram f} : OType (PChain f) :=
   {| oleq := fun s1 s2 => forall n, chain s1 n <o= chain s2 n |}.
 Proof.
@@ -29,31 +43,51 @@ Proof.
   - intros ?; etransitivity; eauto.
 Defined.
 
+(* Project out the nth element of a pchain *)
 Program Definition chainProj {F} `{PDiagram F} n : PChain F -o> F n :=
   {| ofun_app := fun c => chain c n |}.
-Next Obligation. firstorder. Qed.
+Next Obligation. firstorder. Defined.
 
+(* The elements of equivalent chains are themselves equivalent *)
 Lemma chain_inv F `{PDiagram F} (c1 c2 : PChain F) :
   c1 =o= c2 -> forall n, chain c1 n =o= chain c2 n.
 Proof. firstorder. Qed.
 
 
-(** Product diagrams. *)
-Section productPDiagram.
-  Definition ProductTypeSequence (f1 f2 : nat -> Type) : nat -> Type :=
-    fun n => prod (f1 n) (f2 n).
+(** Product diagrams = the product of two type sequences *)
+Definition ProductTypeSequence (f1 f2 : nat -> Type) : nat -> Type :=
+  fun n => prod (f1 n) (f2 n).
 
-  Global Instance ProductOTypeSequence f1 f2
-           {o1 : OTypeSequence f1} {o2 : OTypeSequence f2}
-    : OTypeSequence (ProductTypeSequence f1 f2) :=
-    fun n => OTpair _ _ _ _.
+Instance ProductOTypeSequence f1 f2
+         {o1 : OTypeSequence f1} {o2 : OTypeSequence f2}
+  : OTypeSequence (ProductTypeSequence f1 f2) :=
+  fun n => OTpair _ _ _ _.
 
-  Global Instance ProductPDiagram f1 f2
-         `{PDiagram f1} `{PDiagram f2}
-    : PDiagram (ProductTypeSequence f1 f2) :=
-    fun n => pair_ofun (proj n ∘ fst_ofun) (proj n ∘ snd_ofun).
-End productPDiagram.
-
+Program Instance ProductPDiagram f1 f2 `{PDiagram f1} `{PDiagram f2}
+  : PDiagram (ProductTypeSequence f1 f2) :=
+  {| embed :=
+       fun n => pair_ofun (embed n ∘ fst_ofun) (embed n ∘ snd_ofun);
+     proj :=
+       fun n => pair_ofun (proj n ∘ fst_ofun) (proj n ∘ snd_ofun); |}.
+Next Obligation.
+  unfold ProductTypeSequence, ProductOTypeSequence.
+  rewrite compose_f_pair_ofun.
+  repeat rewrite compose_compose_ofun.
+  rewrite compose_pair_fst. rewrite compose_pair_snd.
+  repeat rewrite <- compose_compose_ofun.
+  repeat rewrite embed_proj_eq. repeat rewrite compose_id_ofun.
+  rewrite pair_fst_snd_eta. reflexivity.
+Defined.
+Next Obligation.
+  unfold ProductTypeSequence, ProductOTypeSequence.
+  rewrite compose_f_pair_ofun.
+  repeat rewrite compose_compose_ofun.
+  rewrite compose_pair_fst. rewrite compose_pair_snd.
+  repeat rewrite <- compose_compose_ofun.
+  repeat rewrite proj_embed_leq. repeat rewrite compose_id_ofun.
+  rewrite pair_fst_snd_eta. reflexivity.
+Defined.
+  
 Lemma product_proper A B `(OType A) `(OType B) (a c : A) (b d : B) :
   a =o= c ->
   b =o= d ->
